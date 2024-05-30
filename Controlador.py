3675d6309b96b1ba2533be644e2350cf6a8b8e4e
wendyo19 <wendyortegaf1f2@gmail.com>
2024-05-30 13:13:24 -0500

    creacion de archivo controlador y vista

---
+++ Controlador.py
@@ -0,0 +1,7 @@
+#Controlador
+from Modelo import *
+from Vista import VentanaPpal
+from PyQt5.QtWidgets import QApplication
+import sys 
+
+
---
+++ Vista.py
@@ -0,0 +1,5 @@
+import sys 
+from PyQt5.QtWidgets import QApplication,QMainWindow, QDialog, QMessageBox,QLineEdit,QTextEdit
+from PyQt5.QtGui import QRegExpValidator, QIntValidator
+from PyQt5.QtCore import Qt,QRegExp
+from PyQt5.uic import loadUi
